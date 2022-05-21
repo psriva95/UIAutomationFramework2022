8e687240049e6c96dc4681fbe50b3bd950fef158
Pooja Srivastava <poojasrivastava95@outlook.com>
2022-05-21 19:20:30 +0530

    orderpage

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+	public void makeOrder() {
+		System.out.println("make order");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
