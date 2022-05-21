aad8c8524dead96799696e6b0cbd72c7438b1818
Pooja Srivastava <101564937+psriva95@users.noreply.github.com>
2022-05-21 19:18:14 +0530

    Create OrderPage.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+
+  public void getOrderId(){
+  
+  }
+	
+  
+  public void makeOrder() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
